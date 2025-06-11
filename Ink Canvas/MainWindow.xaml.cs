cbc317795e153c7e37ae458a0d78e3a21343b0fd
CJK_mkp <113243675+CJKmkp@users.noreply.github.com>
2025-06-11 14:31:20 +0800

    fix:issue #23 #13

--- Ink Canvas/MainWindow.xaml.cs
+++ Ink Canvas/MainWindow.xaml.cs
@@ -150,7 +150,9 @@ namespace Ink_Canvas {
             // 修复“显示画笔光标”选项不可用的问题
             if (Settings.Canvas.IsShowCursor) {
                 inkCanvas1.UseCustomCursor = true;
-                if (inkCanvas1.EditingMode == InkCanvasEditingMode.Ink || drawingShapeMode != 0)
+                // 修复触屏时光标不显示：只要有输入设备悬停就显示
+                if ((inkCanvas1.EditingMode == InkCanvasEditingMode.Ink || drawingShapeMode != 0)
+                    && (inkCanvas1.IsStylusDirectlyOver || inkCanvas1.IsMouseDirectlyOver))
                     inkCanvas1.ForceCursor = true;
                 else
                     inkCanvas1.ForceCursor = false;
