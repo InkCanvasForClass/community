8fc33f5649eab292342491e9adb5fda0b492e5d4
CJKmkp <2564608840@qq.com>
2025-07-28 23:51:14 +0800

    更新软件名称

--- Ink Canvas/Properties/AssemblyInfo.cs
+++ Ink Canvas/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InkCanvasForClass")]
+[assembly: AssemblyTitle("InkCanvasForClass CE")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HARKOTEK Studio")]
-[assembly: AssemblyProduct("InkCanvasForClass")]
+[assembly: AssemblyProduct("InkCanvasForClass CE")]
 [assembly: AssemblyCopyright("© Copyright HARKOTEK Studio 2024-now")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
