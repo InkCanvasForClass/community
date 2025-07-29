53a498c5815f1446a3728925cd24e342a52f60fc
CJKmkp <2564608840@qq.com>
2025-07-29 16:28:22 +0800

    fix:IACore

--- Ink Canvas/Properties/AssemblyInfo.cs
+++ Ink Canvas/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InkCanvasForClass CE")]
+[assembly: AssemblyTitle("InkCanvasForClass")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CJK_mkp")]
-[assembly: AssemblyProduct("InkCanvasForClass CE")]
+[assembly: AssemblyProduct("InkCanvasForClass")]
 [assembly: AssemblyCopyright("© Copyright HARKOTEK Studio 2024-now")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
