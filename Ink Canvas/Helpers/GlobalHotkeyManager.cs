ec0fb0c3cf5050cb2cae43afa685144a67483d87
CJK_mkp <113243675+CJKmkp@users.noreply.github.com>
2025-08-28 18:10:52 +0800

    fix:issue #141

--- Ink Canvas/Helpers/GlobalHotkeyManager.cs
+++ Ink Canvas/Helpers/GlobalHotkeyManager.cs
@@ -767,7 +767,7 @@ namespace Ink_Canvas.Helpers
                     // 在鼠标模式下
                     if (_hotkeysShouldBeRegistered)
                     {
-                        e.Handled = false;
+                        Handled = false;
                     }
                     else
                     {
