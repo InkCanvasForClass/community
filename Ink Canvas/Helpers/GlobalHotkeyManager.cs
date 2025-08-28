f5a08b225c300eb492d538f4ba5ec9b9a2a180db
CJK_mkp <113243675+CJKmkp@users.noreply.github.com>
2025-08-28 18:35:17 +0800

    fix:issue #141

--- Ink Canvas/Helpers/GlobalHotkeyManager.cs
+++ Ink Canvas/Helpers/GlobalHotkeyManager.cs
@@ -28,7 +28,6 @@ namespace Ink_Canvas.Helpers
         {
             _mainWindow = mainWindow ?? throw new ArgumentNullException(nameof(mainWindow));
             _registeredHotkeys = new Dictionary<string, HotkeyInfo>();
-            InitializeHotkeysOnStartup();
         }
         #endregion
 
