f9f73b015cfc50f7d78e452491fe3c895173a48a
CJK_mkp <113243675+CJKmkp@users.noreply.github.com>
2025-11-01 18:31:27 +0800

    improve:启动动画

--- Ink Canvas/Windows/SplashScreen.xaml.cs
+++ Ink Canvas/Windows/SplashScreen.xaml.cs
@@ -393,10 +393,10 @@ namespace Ink_Canvas.Windows
 
                 case 1: // 跟随四季
                     var month = DateTime.Now.Month;
-                    if (month >= 3 && month <= 5) return GetImageNameByStyle(2); // 春季
-                    if (month >= 6 && month <= 8) return GetImageNameByStyle(3); // 夏季
-                    if (month >= 9 && month <= 11) return GetImageNameByStyle(4); // 秋季
-                    return GetImageNameByStyle(5); // 冬季
+                    if (month >= 2 && month <= 4) return GetImageNameByStyle(2); // 春季
+                    if (month >= 5 && month <= 7) return GetImageNameByStyle(3); // 夏季
+                    if (month >= 8 && month <= 10) return GetImageNameByStyle(4); // 秋季
+                    if (month >= 11 || month <= 1) return GetImageNameByStyle(5); // 冬季
 
                 case 2: // 春季
                     return "ICC Spring.png";
