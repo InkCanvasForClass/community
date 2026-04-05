36ff945384c1d08792f086d1bf659a9b638c700f
doudou0720 <98651603+doudou0720@users.noreply.github.com>
2026-04-05 22:54:55 +0800

    fix:修复编译警告
    Signed-off-by: doudou0720 <98651603+doudou0720@users.noreply.github.com>

--- Ink Canvas/AssemblyInfo.cs
+++ Ink Canvas/AssemblyInfo.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows")]
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -14,7 +15,6 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows10.0.19041")]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
