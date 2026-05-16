418dae7ae7ca7c95dab0e0e0cdba900f56fcb378
CJKmkp <2564608840@qq.com>
2026-05-16 17:23:18 +0800

    improve:PPT模块

--- Ink Canvas/Helpers/PPTManager.cs
+++ Ink Canvas/Helpers/PPTManager.cs
@@ -258,6 +258,7 @@ namespace Ink_Canvas.Helpers
 
                     if (!currentSlideShowState)
                     {
+                        SlideShowEnd?.Invoke(CurrentPresentation);
                     }
                 }
             }
