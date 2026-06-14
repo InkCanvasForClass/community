b7c375ea2500985c67cf9afa3d467eb3e4e4d3ec
CJK_mkp <113243675+CJKmkp@users.noreply.github.com>
2026-06-14 12:26:14 +0800

    优化代码

--- Ink Canvas/Windows/SettingsViews/Pages/ToolbarPage.xaml.cs
+++ Ink Canvas/Windows/SettingsViews/Pages/ToolbarPage.xaml.cs
@@ -12,8 +12,8 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Media;
 using System.Windows.Input;
+using System.Windows.Media;
 using Page = iNKORE.UI.WPF.Modern.Controls.Page;
 
 namespace Ink_Canvas.Windows.SettingsViews.Pages
