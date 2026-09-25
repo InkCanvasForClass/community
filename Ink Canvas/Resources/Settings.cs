7dd271e73ab43bff340c7b4844d425bd9ca7e146
NullptrVoid <2963169251@qq.com>
2026-09-25 13:26:10 +0800

    add:增加独立最小化计时器透明开关 (#659)
    * improve:优化计时器逻辑
    
    * add:增加独立最小化计时窗口透明开关
    
    * improve:独立最小化计时器窗口透明度开关支持深色模式

--- Ink Canvas/Resources/Settings.cs
+++ Ink Canvas/Resources/Settings.cs
@@ -56,6 +56,9 @@ namespace Ink_Canvas
         [JsonProperty("notification")]
         public NotificationSettings Notification { get; set; } = new NotificationSettings();
 
+        [JsonProperty("timer")]
+        public TimerSettings Timer { get; set; } = new TimerSettings();
+
         [JsonProperty("toolbar")]
         public ToolbarLayoutSettings Toolbar { get; set; } = new ToolbarLayoutSettings();
 
@@ -298,6 +301,12 @@ namespace Ink_Canvas
 
         [JsonProperty("isDictationDoNotDisturbInWhiteboardEnabled")]
         public bool IsDictationDoNotDisturbInWhiteboardEnabled { get; set; } = true;
+    }
+
+    public class TimerSettings
+    {
+        [JsonProperty("isOpenTransparency")]
+        public bool IsOpenTransparency { get; set; } = true;
     }
 
     public class Security
--- Ink Canvas/Windows/NewStyleMinimizedTimerWindow.xaml.cs
+++ Ink Canvas/Windows/NewStyleMinimizedTimerWindow.xaml.cs
@@ -1,3 +1,5 @@
+using Ink_Canvas.Windows.SettingsViews.Helpers;
+using iNKORE.UI.WPF.Modern;
 using System;
 using System.Timers;
 using System.Windows;
@@ -30,6 +32,10 @@ namespace Ink_Canvas.Windows
             Action stopTimerCallback)
         {
             InitializeComponent();
+            if (!SettingsManager.Settings.Timer.IsOpenTransparency)
+            {
+                MainBorder.Background = (Brush)FindResource("NewTimerWindowBackground");
+            }
             _getRemainingTime = remainingTime;
             _shouldHide = shouldHide;
             _restoreCallback = restoreCallback;
@@ -151,6 +157,22 @@ namespace Ink_Canvas.Windows
         {
             _stopTimerCallback?.Invoke();
             Close();
+        }
+
+        private void ToggleTransparency_Click(object sender, RoutedEventArgs e)
+        {
+            if(SettingsManager.Settings.Timer.IsOpenTransparency)
+            {
+                MainBorder.Background = (Brush)FindResource("NewTimerWindowBackground");
+                SettingsManager.Settings.Timer.IsOpenTransparency = false;
+                SettingsManager.SaveSettingsToFile();
+            }
+            else
+            {
+                MainBorder.Background = (Brush)FindResource(ThemeKeys.CardBackgroundFillColorDefaultBrushKey);//还原
+                SettingsManager.Settings.Timer.IsOpenTransparency = true;
+                SettingsManager.SaveSettingsToFile();
+            }
         }
     }
 }
